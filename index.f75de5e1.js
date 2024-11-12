788d950f23cbcc8e7786f38b2a9cfbd9b8330cfa
Alena Sokol <alenashpakova27021995@gmail.com>
2024-11-12 22:49:58 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,21 @@
+"use strict";
+const populations = [
+    ...document.querySelectorAll("span.population")
+];
+// const onePopulation = document.queryElementById
+const mappedPopulation = [];
+populations.forEach((amount)=>{
+    const number = amount.textContent.split("").filter((el)=>el !== ",").join("");
+    mappedPopulation.push(+number);
+});
+const formattedNumberUS = (input)=>{
+    return input.toLocaleString("en-US");
+};
+const total = mappedPopulation.reduce((accumulator, curentValue)=>accumulator + curentValue, 0);
+const average = total / mappedPopulation.length;
+const totalPopulation = document.querySelector(".total-population");
+totalPopulation.textContent = formattedNumberUS(total);
+const averagePopulation = document.querySelector(".average-population");
+averagePopulation.textContent = formattedNumberUS(average);
+
+//# sourceMappingURL=index.f75de5e1.js.map
